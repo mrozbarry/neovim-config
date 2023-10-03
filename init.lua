dcebc706a1e6b7b84890026e598d6d478762daf2
Alex Barry <alex.barry@gmail.com>
2023-10-03 15:23:09 -0400

    Fix warning for indent-blankline

--- init.lua
+++ init.lua
@@ -114,24 +114,24 @@ require('lazy').setup({
   },
 
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
       -- char = '┊',
-      space_char_blankline = " ",
-      show_current_context = true,
-      show_current_context_start = true,
-      show_trailing_blankline_indent = false,
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-      },
+      -- space_char_blankline = " ",
+      -- show_current_context = true,
+      -- show_current_context_start = true,
+      -- show_trailing_blankline_indent = false,
+      -- char_highlight_list = {
+      --   "IndentBlanklineIndent1",
+      --   "IndentBlanklineIndent2",
+      --   "IndentBlanklineIndent3",
+      --   "IndentBlanklineIndent4",
+      --   "IndentBlanklineIndent5",
+      --   "IndentBlanklineIndent6",
+      -- },
     },
   },
 
